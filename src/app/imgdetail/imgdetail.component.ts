d68ff4cbb47963a0bc1f6522891742f09436b463
yuk <yukarinrin@163.com>
2017-12-02 12:08:44 +0800

    可以选择，但细节图并没有的到展示数据，下一步需建立服务，传递展示数据

--- src/app/imgdetail/imgdetail.component.ts
+++ src/app/imgdetail/imgdetail.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-imgdetail',
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./imgdetail.component.scss']
 })
 export class ImgdetailComponent implements OnInit {
-
+  @Input()selected;
   constructor() { }
 
   ngOnInit() {
--- src/app/imglist/imglist.component.ts
+++ src/app/imglist/imglist.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewEncapsulation, Input } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation, Input, ViewChild, TemplateRef } from '@angular/core';
 
 @Component({
 
@@ -7,20 +7,60 @@ import { Component, OnInit, ViewEncapsulation, Input } from '@angular/core';
   styleUrls: ['./imglist.component.scss']
 })
 export class ImglistComponent implements OnInit {
-  rows = [
-    { name: 'Austin', gender: 'Male', company: 'Swimlane' },
-    { name: 'Dany', gender: 'Male', company: 'KFC' },
-    { name: 'Molly', gender: 'Female', company: 'Burger King' },
-  ];
-  columns = [
-    { prop: 'name' },
-    { name: 'Gender' },
-    { name: 'Company' }
-  ];
+  @ViewChild('editTmplImg') editTmplImg: TemplateRef<any>;
+  @ViewChild('hdrTpl') hdrTpl: TemplateRef<any>;
+  @ViewChild('editTmplName') editTmplName: TemplateRef<any>;
+  //@ViewChild('hdrTplName') hdrTplName: TemplateRef<any>;
+  @ViewChild('editTmpl') editTmpl: TemplateRef<any>;
+  //@ViewChild('hdrTplType') hdrTplType: TemplateRef<any>;
+  @ViewChild('editTmplSize') editTmplSize: TemplateRef<any>;
+  //@ViewChild('hdrTplSize') hdrTplSize: TemplateRef<any>;
   @Input() dackMode = false;
-  constructor() { }
+  rows = [];
+  columns = [];
+
+  constructor() {
+    this.fetch((data) => {
+      this.rows = data;
+    });
+  }
 
   ngOnInit() {
+    this.columns = [{
+      cellTemplate: this.editTmplImg,
+      headerTemplate: this.hdrTpl,
+      name: 'imglist',
+      prop: 'src'
+    },
+    {
+      cellTemplate: this.editTmpl,
+      headerTemplate: this.hdrTpl,
+      name: 'name'
+    },
+    {
+      cellTemplate: this.editTmpl,
+      headerTemplate: this.hdrTpl,
+      name: 'type'
+    },
+    {
+      cellTemplate: this.editTmpl,
+      headerTemplate: this.hdrTpl,
+      name: 'size'
+    }
+  ];
+  }
+  onSelect({ selected }) {
+    console.log('Select Event', selected);
+  }
+  fetch(cb) {
+    const req = new XMLHttpRequest();
+    req.open('GET', `assets/data/imglist.json`);
+
+    req.onload = () => {
+      cb(JSON.parse(req.response));
+    };
+
+    req.send();
   }
 
 }
